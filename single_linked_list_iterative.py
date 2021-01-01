298b1d0fd87fd83588fdf82f2f09bf4a960cd116
Michelle Shelby <michelleshelby76@gmail.com>
2021-01-01 09:08:26 -0600

    added print statements; clarified remove requests

--- single_linked_list_iterative.py
+++ single_linked_list_iterative.py
@@ -1,3 +1,6 @@
+# This is a single linked list. 
+# Add and Remove nodes to the list
+
 class Node(object): # this is a definition of a class 'Node'
     def __init__(self, d, n = None): # Initializes the 'Node' object with parameters of data & next node
         self.data = d # setting 'data' attribute to the 'd' parameter
@@ -17,10 +20,12 @@ class LinkedList (object): # this is a definition of a class 'LinkedList'
     def get_size (self): # retrieves the length of the list
         return self.size
     def add (self, d): # creates new node and pushes it to the list
+        print('adding node...')
         new_node = Node (d, self.root) # initializing a 'new_node' with 'd' as the data and root as the next_node
         self.root = new_node
         self.size += 1 # increments the length of the list
     def remove (self, d): # removes node based on the data passed in from the parameter
+        print('removing node...')
         this_node = self.root
         prev_node = None
         while this_node: # as long as there's a node...
@@ -31,25 +36,28 @@ class LinkedList (object): # this is a definition of a class 'LinkedList'
                 else: # node's data does not match the 'd' parameter...
                     self.root = this_node.get_next() # assigns the root to the node that follows this one
                 self.size -= 1 # decrements the length of the list
+                print('The node was removed.')
                 return True # the node was successfully removed
             else:
                 prev_node = this_node # assigns this_node to now be the prev_node
                 this_node = this_node.get_next() # assigns the next_node to now be this_node
+        print('No node found. No node removed.')
         return False # data wasn't found; no node removed
     def find (self, d): # finds node based on the data passed in from the parameter
+        print('finding node...')
         this_node = self.root
         while this_node: # as long as there's a node...
             if this_node.get_data() == d: # checks to see if the node's data matches the 'd' parameter
                 return d # displays the data
             else: # node's data does not match the 'd' parameter...
                     this_node = this_node.get_next() # assigns the next_node to now be this_node
+        print('No node found.')
         return None # data wasn't found; no node found
-
+            
 myList = LinkedList() # initalizing a new list called 'myList'
 myList.add("Breakfast") # adding "Breakfast" to myList
 myList.add("Lunch")
 myList.add("Dinner")
-myList.remove("Dinner") # removing "Dinner" from myList
 print(myList.remove("Dinner")) # displaying the result of removing "Dinner" from myList
 print(myList.find("Lunch")) # displaying the result of finding "Lunch" from myList
 print(myList.find("Dinner"))
